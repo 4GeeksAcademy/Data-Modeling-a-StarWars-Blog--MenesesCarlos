314836b04f4afd9e8b43cb5e758d43af1a27ea5c
Carlos Meneses <carlosocharly1@hotmail.com>
2025-10-21 21:37:13 +0000

    Data Modeling a StarWars Blog-MenesesCarlos

--- src/models.py
+++ src/models.py
@@ -1,19 +1,80 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+import enum
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Character(db.Model):
+    __tablename__ = 'character'
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
+    description: Mapped[str] = mapped_column(db.String(1000),nullable=True)
+    image_url: Mapped[str] = mapped_column(db.String(250),nullable=True)
+    name: Mapped[str] = mapped_column(db.String(50),nullable=False)
+    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "description": self.description,
         }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image_url: Mapped[str] = mapped_column(db.String(250),nullable=True)
+    name: Mapped[str] = mapped_column(db.String(50),nullable=False)
+    population: Mapped[int] = mapped_column(nullable=True)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+        }
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    email: Mapped[str] = mapped_column(db.String(120), nullable=False)
+    first_name: Mapped[str] = mapped_column(db.String(50), nullable=True)
+    is_active: Mapped[bool] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(db.String(50), nullable=True)
+    password: Mapped[str] = mapped_column(db.String(80), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now, nullable=False)
+    user_name: Mapped[str] = mapped_column(db.String(50), nullable=False)
+    
+    favorites: Mapped[List["Favorite"]] = relationship(back_populates="user_item")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.user_name,
+            "email": self.email,
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    user_id: Mapped[int] = mapped_column(db.ForeignKey("user.id"), nullable=False)
+    planet_id: Mapped[int] = mapped_column(db.ForeignKey("planet.id"), nullable=True)
+    character_id: Mapped[int] = mapped_column(db.ForeignKey("character.id"), nullable=True)
+    
+    user_item: Mapped["User"] = relationship(back_populates="favorites")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
+        }
+
+class AlembicVersion(db.Model):
+    __tablename__ = 'alembic_version'
+    version_num: Mapped[str] = mapped_column(db.String(32),primary_key=True)
+
